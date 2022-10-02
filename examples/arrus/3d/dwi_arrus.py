1e45dbac1ea7bd31d832a622a937b4ad95608489
Piotr Jarosik <piotr.jarosik@us4us.eu>
2022-10-02 19:45:44 +0200

    Renamed examples, updated DWI example.

--- examples/arrus/3d/dwi_arrus.py
+++ examples/arrus/3d/dwi_arrus.py
@@ -1,13 +1,14 @@
+import sys
 import arrus
 import arrus.session
 import arrus.utils.imaging
 import arrus.utils.us4r
 import pickle
-from datetime import datetime
-
+from parameters import *
+from arrus.ops.us4r import *
 from arrus.utils.imaging import *
 from arrus.utils.gui import Display2D
-from parameters import *
+from datetime import datetime
 
 arrus.set_clog_level(arrus.logging.INFO)
 arrus.add_log_file("test.log", arrus.logging.INFO)
@@ -15,17 +16,17 @@ arrus.add_log_file("test.log", arrus.logging.INFO)
 
 def main():
     # Here starts communication with the device.
-    with arrus.Session("us4r.prototxt") as sess:
+    with arrus.Session("./us4r.prototxt") as sess:
         us4r = sess.get_device("/Us4R:0")
         us4r.set_hv_voltage(tx_voltage)
         probe_model = us4r.get_probe_model()
 
-        tx_focus = [np.inf]
+        tx_focus = [-20e-3]
         tx_ang_zx = [0]
         tx_ang_zy = [0]
 
         seq = get_sequence(
-            probe_model=probe_model,
+            probe_model,
             tx_focus=tx_focus, tx_ang_zx=tx_ang_zx, tx_ang_zy=tx_ang_zy)
 
         processing, rf_queue = get_imaging(
@@ -37,20 +38,21 @@ def main():
             processing=processing
         )
         us4r.set_tgc((tgc_t, tgc_values))
+        # Upload the scheme on the us4r-lite device.
         buffer, metadata = sess.upload(scheme)
-        display = Display2D(metadata=metadata,
-                            value_range=(20, 80), cmap="gray",
-                            title="B-mode", xlabel="OX (mm)", ylabel="OZ (mm)",
+        # Created 2D image display.
+        display = Display2D(metadata=metadata, value_range=(-80, 0), cmap="gray",
+                            title="B-mode", xlabel="OX+OY (px)",
+                            ylabel="OZ (px)",
                             show_colorbar=True)
         # Start the scheme.
-        input("Press any key to continue")
         sess.start_scheme()
         # Start the 2D display.
+        # The below function blocks current thread until the window is closed.
         display.start(buffer)
         data = np.stack(rf_queue)
         current_date = datetime.today().strftime('%Y-%m-%d')
-        # The below function blocks current thread until the window is closed.
-        filename = f"pwi_3d_{current_date}"
+        filename = f"dwi_3d_{current_date}"
         pickle.dump({"rf": data, "metadata": metadata},
                     open(f"{filename}.pkl", "wb"))
         print("Display closed, stopping the script.")
--- examples/arrus/3d/pwi_arrus.py
+++ examples/arrus/3d/pwi_arrus.py
@@ -1,14 +1,13 @@
-import sys
 import arrus
 import arrus.session
 import arrus.utils.imaging
 import arrus.utils.us4r
 import pickle
-from parameters import *
-from arrus.ops.us4r import *
+from datetime import datetime
+
 from arrus.utils.imaging import *
 from arrus.utils.gui import Display2D
-from datetime import datetime
+from parameters import *
 
 arrus.set_clog_level(arrus.logging.INFO)
 arrus.add_log_file("test.log", arrus.logging.INFO)
@@ -16,32 +15,42 @@ arrus.add_log_file("test.log", arrus.logging.INFO)
 
 def main():
     # Here starts communication with the device.
-    with arrus.Session("./us4r.prototxt") as sess:
+    with arrus.Session("us4r.prototxt") as sess:
         us4r = sess.get_device("/Us4R:0")
         us4r.set_hv_voltage(tx_voltage)
         probe_model = us4r.get_probe_model()
-        seq = get_dwi_sequence(probe_model)
-        processing, rf_queue = get_dwi_imaging(sequence=seq)
+
+        tx_focus = [np.inf]
+        tx_ang_zx = [0]
+        tx_ang_zy = [0]
+
+        seq = get_sequence(
+            probe_model=probe_model,
+            tx_focus=tx_focus, tx_ang_zx=tx_ang_zx, tx_ang_zy=tx_ang_zy)
+
+        processing, rf_queue = get_imaging(
+            sequence=seq,
+            tx_focus=tx_focus, tx_ang_zx=tx_ang_zx, tx_ang_zy=tx_ang_zy)
         # Declare the complete scheme to execute on the devices.
         scheme = Scheme(
             tx_rx_sequence=seq,
             processing=processing
         )
         us4r.set_tgc((tgc_t, tgc_values))
-        # Upload the scheme on the us4r-lite device.
         buffer, metadata = sess.upload(scheme)
-        # Created 2D image display.
-        display = Display2D(metadata=metadata, value_range=(20, 80), cmap="gray",
-                            title="B-mode", xlabel="OX+Y (mm)", ylabel="OZ (mm)",
+        display = Display2D(metadata=metadata,
+                            value_range=(-80, 0), cmap="gray",
+                            title="B-mode", xlabel="OX (mm)", ylabel="OZ (mm)",
                             show_colorbar=True)
         # Start the scheme.
+        input("Press any key to continue")
         sess.start_scheme()
         # Start the 2D display.
-        # The below function blocks current thread until the window is closed.
         display.start(buffer)
         data = np.stack(rf_queue)
         current_date = datetime.today().strftime('%Y-%m-%d')
-        filename = f"dwi_3d_{current_date}"
+        # The below function blocks current thread until the window is closed.
+        filename = f"pwi_3d_{current_date}"
         pickle.dump({"rf": data, "metadata": metadata},
                     open(f"{filename}.pkl", "wb"))
         print("Display closed, stopping the script.")
