5420c66afe25ac8eae896e976f02ce255e98dadc
Piotr Jarosik <piotr.jarosik@us4us.eu>
2023-07-18 10:56:43 +0200

    Refactored folder tree structure.

--- examples/3d/test_parameters.py
+++
@@ -1,9 +0,0 @@
-import parameters
-import numpy as np
-import matplotlib.pyplot as plt
-
-delays = parameters.get_delays_raw([-10e-3], [-10 * np.pi / 180], [-10 * np.pi / 180], 0.5e-3, 1024, 1450)
-delays = delays.reshape((-1, 32, 32))
-plt.imshow(delays[0])
-plt.colorbar()
-plt.show()
--- examples/image_enhancement/nn_bmode/__init__.py
+++
@@ -1,1 +0,0 @@
-from nn_bmode.model import NNBmode
