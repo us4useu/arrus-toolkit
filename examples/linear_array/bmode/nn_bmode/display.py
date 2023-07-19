9cb6f4170368697cb77a007b6ced5869752e5afe
Piotr Jarosik <piotr.jarosik@us4us.eu>
2023-07-19 10:28:16 +0200

    Implemented NN-Bmode example.

--- examples/linear_array/bmode/nn_bmode/display.py
+++ examples/linear_array/bmode/nn_bmode/display.py
@@ -4,13 +4,23 @@ from gui4us.cfg.display import *
 # Display configuration file.
 
 displays = {
-    "OX": Display2D(
+    "BMODE": Display2D(
         title=f"B-mode",
         layers=(
             Layer2D(
                 value_range=(20, 80),
                 cmap="gray",
                 input=StreamDataId("default", 0),
+            ),
+        ),
+    ),
+    "NN-B-mode": Display2D(
+        title=f"NN-B-mode",
+        layers=(
+            Layer2D(
+                value_range=(100, 200),
+                cmap="gray",
+                input=StreamDataId("default", 1),
             ),
         ),
     ),
