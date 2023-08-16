02a58043f095f5be0bc41dfc9843944ed604edfe
Piotr Jarosik <jarosik.piotr@gmail.com>
2023-08-16 18:34:10 +0200

    Extracted common code to separate utils package.

--- examples/rca/holo_display/probe_params.py
+++ examples/rca/holo_display/probe_params.py
@@ -1,28 +1,4 @@
-from dataclasses import dataclass
-from arrus.devices.probe import ProbeModel, ProbeModelId
-
-
-@dataclass(frozen=True)
-class ProbeArray:
-    """
-    RCA probe array description.
-
-    :param n_elements: number of elements in the given array
-    :param pitch: the distance between consecutive elements
-    :param start: the number of system channel connected to the first element
-       of the array
-    """
-    n_elements: int
-    pitch: float
-    start: int
-    arrangement: str
-
-    def to_arrus_probe(self):
-        return ProbeModel(
-            model_id=ProbeModelId("us4us", f"RCA_{self.start}"),
-            n_elements=self.n_elements,
-            pitch=self.pitch, curvature_radius=0
-        )
+from arrus_rca_utils.probe_params import *
 
 
 # DEMO parameters: Vermon 128+128 elements
---
+++ examples/rca/utils/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="arrus_rca_utils",
+    version="0.1.0",
+    author="us4us Ltd.",
+    author_email="support@us4us.eu",
+    description="Utility functions to work with ARRUS and RCA probe",
+    long_description="Utility functions to work with ARRUS and RCA probe",
+    long_description_content_type="text/markdown",
+    url="https://us4us.eu",
+    packages=setuptools.find_packages(exclude=[]),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 1 - Planning",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Embedded Systems",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Medical Science Apps."
+    ],
+    python_requires='>=3.8'
+)
