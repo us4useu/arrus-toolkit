28811acb1c4c12d2a946205c2a46e6f732823164
Piotr Jarosik <piotr.jarosik@us4us.eu>
2023-08-22 20:39:45 +0200

    #ARRUS-226: Implemented Color Doppler example. (#3)

---
+++ examples/linear_array/color_doppler/display.py
@@ -0,0 +1,41 @@
+from gui4us.model import *
+from gui4us.cfg.display import *
+
+BMODE_DRANGE = (60, 120)
+COLOR_DRANGE = (-1.57, 1.57)
+POWER_DRANGE = (50, 80)
+# Display configuration file.
+displays = {
+    "ColorDoppler": Display2D(
+        title=f"Color Doppler",
+        layers=(
+            Layer2D(
+                value_range=BMODE_DRANGE,
+                cmap="gray",
+                input=StreamDataId("default", 0),
+            ),
+            Layer2D(
+                value_range=COLOR_DRANGE,
+                cmap="bwr",
+                input=StreamDataId("default", 1),
+            ),
+        ),
+    ),
+    "PowerDoppler": Display2D(
+        title=f"Power Doppler",
+        layers=(
+            Layer2D(
+                value_range=BMODE_DRANGE,
+                cmap="gray",
+                input=StreamDataId("default", 0),
+            ),
+            Layer2D(
+                value_range=POWER_DRANGE,
+                cmap="hot",
+                input=StreamDataId("default", 2),
+            ),
+        ),
+    ),
+}
+
+VIEW_CFG = ViewCfg(displays)
